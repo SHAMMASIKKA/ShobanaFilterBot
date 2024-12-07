7fa185a9085128a3a4081349caedb1b4289fa618
SHAMMASIKKA <mazffx8@gmail.com>
2024-12-07 07:47:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GouthamSER/KuttuBot.git /KuttuBot
+  git clone https://github.com/SHAMMASIKKA/ShobanaFilterBot.git /ShobanaFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /KuttuBot
+  git clone $UPSTREAM_REPO /ShobanaFilterBot
 fi
-cd /KuttuBot
+cd /ShobanaFilterBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
